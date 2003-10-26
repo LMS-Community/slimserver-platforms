66886dd6ddf0ede4993ae8f3de206be6219c8b2c
Dave Nanian <dnanian@pobox.com>
2003-10-26 19:32:31 +0000

    Added stand-alone firmware updater.
    git-svn-id: file:///home/awy/mirror/slim/trunk/platforms@243 62299810-d8cb-41fd-93b7-d32162e5a4a4

---
+++ osx/Preference Pane/SLIMP3 Firmware Updater/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Installer
+//
+//  Created by Dave Nanian on Fri Jan 03 2003.
+//  Copyright (c) 2003 Slim Devices, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
