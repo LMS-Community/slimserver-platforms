e93dc676223bebacb122c3503614d71b3c7206d5
Dean Blackketter <dean@slimdevices.com>
2004-07-25 22:47:38 +0000

    Michael Herger submitted some patches to the Handheld skin a while back.  Finally got to them.
    A few folks have requested the display of the name of the playlist currently playing.  We now display this on the EN, Default and Handheld skins.  If it gets changed,
    it adds "(Modified)", if the playlist was created from scratch, it's not displayed at all.
    
    Dave Nanian submitted a patch to create the logs folder if necessary on OSX.
    
    
    git-svn-id: file:///home/awy/mirror/slim/trunk/platforms@1121 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/Start Slim Server.sh
+++ osx/Preference Pane/Start Slim Server.sh
@@ -5,6 +5,7 @@ if [ z"$SERVER_RUNNING" = z ] ; then
     # Check to see if we're publishing with Rendezvous -- if not, use daemon mode.
     
     if [ z"$#" == z"0" ] ; then
+	if [ ! -e ~/Library/Logs ] ; then mkdir ~/Library/Logs ; fi
 	./slimserver.pl --daemon --d_server --logfile ~/Library/Logs/slimserver.log >> ~/Library/Logs/slimserver.log 2>&1
     else
 	./slimserver.pl >> /tmp/slimerror.log 2>&1 
