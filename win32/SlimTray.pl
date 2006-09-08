0fec130384f05f96e171d302dcc854d5c2c3f76a
Dan Sully <dsully@slimdevices.com>
2006-09-08 15:37:03 +0000

    Revert patch until we can figure out what's going on.
    git-svn-id: file:///home/awy/mirror/slim/trunk/platforms@9494 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- win32/SlimTray.pl
+++ win32/SlimTray.pl
@@ -1,16 +1,12 @@
-#!/usr/bin/perl -w
-
-use strict;
 use PerlTray;
+
 use Win32::Service;
-use Win32::Process::Info;
 use Getopt::Long;
 
-my $ssActive          = 0;
+my $ssActive = 0;
 my $pendingActivation = 0;
-my $scanning          = 0;
-my $start             = 0;
-my $exit              = 0;
+my $start = 0;
+my $exit = 0;
 
 use constant DEFAULT_TIMER_INTERVAL => 10;
 
@@ -33,19 +29,17 @@ sub PopupMenu {
 	push @menu, ["--------"];
 	push @menu, ["Go to Slim Devices Web Site", "Execute 'http://www.slimdevices.com'"];
 	push @menu, ["E&xit", "exit"];
-
+	
 	return \@menu;
 }
 
 # Called when the tray application is invoked again. This can handle
 # new startup parameters.
 sub Singleton {
-
 	# Had problems using Getopt::Long since @ARGV isn't set.
 	# XXX There also seems to be a problem with arguments passed
 	# in. $_[0] is not the first parameter, so we use $_[1].
 	if (scalar(@_) > 1) {
-
 		if ($_[1] eq '--start') {
 			startSlimServer();
 		}
@@ -57,18 +51,14 @@ sub Singleton {
 
 # Display tooltip based on SS state
 sub ToolTip {
-
 	if ($pendingActivation) {
 		return "SlimServer Starting";
 	}
 
-	if ($ssActive and $scanning) {
-		return "SlimServer Running (Scanning)";
-	}
-	elsif ($ssActive) {
+	if ($ssActive) {
 		return "SlimServer Running";
 	}
-
+   
 	return "SlimServer Stopped";
 }
 
@@ -82,7 +72,6 @@ sub Timer {
 	if ($pendingActivation) {
 		SetAnimation(DEFAULT_TIMER_INTERVAL * 1000, 1000, "SlimServer", "SlimServerOff");
 	}
-
 	# If we were waiting for SS to start before this check,
 	# show the SS home page.
 	# XXX Need to find actual HTTP port in prefs.
@@ -99,7 +88,7 @@ sub checkAndStart {
 	exit if ($exit);
 
 	checkSSActive();
-
+	
 	if ($ssActive) {
 		SetIcon("SlimServer");
 	}
@@ -108,73 +97,45 @@ sub checkAndStart {
 	}
 
 	if ($start) {
-
 		if (!$ssActive) {
 			startSlimServer();
-		} else {
+		}
+		else {
 			Execute("http://localhost:9000");
 		}
-
 		$start = 0;
 	}
 }
 
-sub checkSSActive {
-	my %status = ();
 
-	# Use the Win32::Service package to see if the service is still active.
+sub checkSSActive {
+	my %status;
+
+	# We use the Win32::Service package to see if the service
+	# is still active. If we wanted the tray app to work with
+	# Slimserver running as an application, we could attempt to
+	# connect to the CLI port, e.g.:
+	#   $sock = IO::Socket::INET->new(PeerAddr => 'localhost',
+	#								  PeerPort => 9090,
+	#								  Proto => 'tcp',
+	#								  Reuse => 1,
+	#								  Timeout => 9);
+
 	Win32::Service::GetStatus('', 'slimsvc', \%status);
 
 	if ($status{'CurrentState'} == 0x04) {
-
 		SetIcon("SlimServer");
 		$ssActive = 1;
 		$pendingActivation = 0;
-
-	} else {
-
-		# Look for the SlimServer CLI, if we're running as an application.
-		my $sock = IO::Socket::INET->new(
-			PeerAddr => 'localhost',
-			PeerPort => 9090,
-			Proto    => 'tcp',
-			Reuse    => 1,
-			Timeout  => 9
-		);
-
-		if (defined($sock) && fileno($sock)) {
-
-			SetIcon("SlimServer");
-			$ssActive = 1;
-			$pendingActivation = 0;
-			close($sock);
-
-		} else {
-
-			SetIcon("SlimServerOff");
-			$ssActive = 0;
-		}
 	}
-
-	# Look for the scanner process
-	my $pi   = Win32::Process::Info->new();
-	my @info = $pi->GetProcInfo();
-
-	$scanning = 0;
-
-	for my $pid (@info) {
-
-		if ($pid->{'Name'} eq "scanner.exe") {
-
-			$scanning = 1;
-			last;
-		}
+	else {
+		SetIcon("SlimServerOff");
+		$ssActive = 0;
 	}
 }
 
 sub startSlimServer {
-
-	if (!$ssActive) {
+	unless ($ssActive) {
 		Balloon("Starting SlimServer...", "SlimServer", "", 1);
 		SetAnimation(DEFAULT_TIMER_INTERVAL * 1000, 1000, "SlimServer", "SlimServerOff");
 	}
@@ -185,7 +146,6 @@ sub startSlimServer {
 }
 
 sub stopSlimServer {
-
 	if ($ssActive) {
 		Balloon("Stopping SlimServer...", "SlimServer", "", 1);
 	}
@@ -197,10 +157,8 @@ sub stopSlimServer {
 
 *PerlTray::ToolTip = \&ToolTip;
 
-GetOptions(
-	'start' => \$start,
-	'exit'  => \$exit
-);
+GetOptions('start' => \$start,
+		   'exit' => \$exit);
 
 # Checking for existence & launching of SS in a timer, since it
 # fails if done during Perl initialization.
