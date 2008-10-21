7174f45d798dd7c1c69d587e54bafe46c179a156
Michael Herger <mherger@logitech.com>
2008-10-21 15:13:38 +0000

    open SC in Safari instead of showing the preference pane
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/platforms@23646 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/Installer/Postflight.sh
+++ osx/Preference Pane/Installer/Postflight.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-# Under Panther, it takes a while for LaunchServices to prep the prefPane for
-# startup. So, we take a new approach to starting it. Basically, we manually
-# start System Preferences, and then ask it to load it.
+# try to detect SC's web port, and verify whether it's available or not
+# don't try more than 15 seconds
+# open SC web UI in Safari, if SC is available
 
-# 10.1 doesn't have a scriptable system preferences application, so we ask
-# it by starting it manually, and then using open. Later versions allow
-# a direct request.
-
-if /usr/bin/sw_vers | grep "10.1" >/dev/null; then
-	osascript <<ENDSCRIPT
-		tell application "System Preferences" to activate
-ENDSCRIPT
-	open $1
+if [ -e "$HOME/Library/PreferencePanes/SqueezeCenter.prefPane/Contents/Resources" ] ; then
+	cd "$HOME/Library/PreferencePanes/SqueezeCenter.prefPane/Contents/Resources"
 else
+	cd "/Library/PreferencePanes/SqueezeCenter.prefPane/Contents/Resources"
+fi
+
+PORT=`./check-web.pl`
+i=0
+
+while [ $PORT -eq 0 ] && [ $i -lt 15 ]
+do
+	i=`expr $i + 1`
+	sleep 1
+	
+	PORT=`./check-web.pl`	
+done
+
+if [ $PORT -ne 0 ]; then
 	osascript <<ENDSCRIPT
-	tell application "System Preferences"
-		set current pane to pane "com.slimdevices.slim"
-		activate
-	end tell
+		tell application "Safari" 
+			activate
+			open location "http://localhost:$PORT/"
+		end tell
 ENDSCRIPT
 fi
