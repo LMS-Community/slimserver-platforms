b89bdee7311dbd34ea7c6112b0fb0a7b62acf238
Michael Herger <michael@slimdevices.com>
2009-02-18 08:26:27 +0000

    fix updating versions on top of each other
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@25049 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- win32/WHS Add-In/SqueezeCenter/installer.bat
+++ win32/WHS Add-In/SqueezeCenter/installer.bat
@@ -1,3 +1,2 @@
-﻿
 "..\..\WiX\candle.exe" "installer.wxs" -out "bin\Release\install.wixobj"
-"..\..\WiX\light.exe" "bin\Release\install.wixobj" -out "bin\Release\SqueezePanel.0.0.0.7.msi"
+"..\..\WiX\light.exe" "bin\Release\install.wixobj" -out "bin\Release\SqueezePanel-7.3.3-125.msi"
