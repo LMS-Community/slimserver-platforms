773c3817e2cd9157c64ebe8f2f782aecf82ff4db
Michael Herger <michael@slimdevices.com>
2009-02-19 14:54:17 +0000

    WHS add-ins can't be updated. They have to be uninstalled before an update can be installed. This sucks, but this is how MS wants us to do this. Thus add a message telling the user to uninstall first if he tries to run an update.
    Bug: 11055
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@25086 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- win32/WHS Add-In/SqueezeCenter/installer.bat
+++ win32/WHS Add-In/SqueezeCenter/installer.bat
@@ -1,2 +1,2 @@
 "..\..\WiX\candle.exe" "installer.wxs" -out "bin\Release\install.wixobj"
-"..\..\WiX\light.exe" "bin\Release\install.wixobj" -out "bin\Release\SqueezePanel-7.3.3-125.msi"
+"..\..\WiX\light.exe" "bin\Release\install.wixobj" -out "bin\Release\SqueezePanel-7.4-142.msi"
