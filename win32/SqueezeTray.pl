cb121c6a8d9c3a6075a8cfce5db2b65a176379c2
Michael Herger <michael@slimdevices.com>
2009-03-08 05:35:06 +0000

    refactor SqueezeTray to share more code available to other scripts (control panel)
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@25401 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- win32/SqueezeTray.pl
+++ win32/SqueezeTray.pl
@@ -1,35 +1,23 @@
 # $Id$
 # 
 # SqueezeTray.exe controls the starting & stopping of the SqueezeCenter application
-#
-# This program relies on Win32::Daemon, which is not part of CPAN.
-# http://www.roth.net/perl/Daemon/
 
 use strict;
 use PerlTray;
 
-use Cwd qw(cwd);
-use File::Spec;
+use File::Spec::Functions qw(catdir);
 use Getopt::Long;
 use Socket;
 use Encode;
 
-use Win32 qw(GetOSName);
 use Win32::Locale;
-use Win32::Process qw(DETACHED_PROCESS CREATE_NO_WINDOW NORMAL_PRIORITY_CLASS);
-use Win32::Process::List;
-use Win32::TieRegistry ('Delimiter' => '/');
-use Win32::Service;
 
-my $timerSecs      = 10;
-my $scActive       = 0;
-my $starting       = 0;
-my $processObj     = 0;
-my $checkHTTP      = 0;
-my $lastHTTPPort   = 0;
-my $nextUpdateCheck= 0;
+use constant SLIM_SERVICE => 0;
+use constant SCANNER => 0;
+use constant TIMERSECS => 10;
 
-my %strings        = ();
+use Slim::Utils::OS::Win32::ServiceMgr;
+use Slim::Utils::Light;
 
 # Passed on the command line by Getopt::Long
 my $cliStart       = 0;
@@ -37,59 +25,41 @@ my $cliExit        = 0;
 my $cliInstall     = 0;
 my $cliUninstall   = 0;
 
-my $registryKey    = 'CUser/Software/Logitech/SqueezeCenter';
-
-# Migrate SlimServer settings
-if (my $ssRegistryKey  = 'CUser/Software/SlimDevices/SlimServer') {
-	if (defined $Registry->{"$ssRegistryKey/StartAtLogin"}) {
-		$Registry->{"$registryKey/StartAtLogin"} = $Registry->{"$ssRegistryKey/StartAtLogin"};
-		delete $Registry->{"$ssRegistryKey/StartAtLogin"};
-	}
-
-	delete $Registry->{"$ssRegistryKey/"};
-	delete $Registry->{'CUser/Software/SlimDevices/'};
-}
-
-my $atLogin        = $Registry->{"$registryKey/StartAtLogin"};
-
-my $serviceName    = 'squeezesvc';
-
-my $appExe         = File::Spec->catdir(installDir(), 'server', 'squeezecenter.exe');
-my $serverPrefFile = File::Spec->catdir(writableDir(), 'prefs', 'server.prefs');
 my $language       = getPref('language') || 'EN';
 
 # Dynamically create the popup menu based on SqueezeCenter state
 sub PopupMenu {
 	my @menu = ();
+	
+	my $type = getStartupType();
+	my $state = getServiceState();
 
-	my $type = startupType(); # = none, login or auto
-
-	if ($type eq 'auto') {
-		push @menu, [sprintf('*%s', string('OPEN_SQUEEZECENTER')), $scActive ? \&openSqueezeCenter : undef];
+	if ($type == SC_STARTUP_TYPE_SERVICE) {
+		push @menu, [sprintf('*%s', string('OPEN_SQUEEZECENTER')), $state == SC_STATE_RUNNING ? \&openSqueezeCenter : undef];
 		push @menu, ["--------"];
-		push @menu, [string('STOP_SQUEEZECENTER'), $scActive ? \&stopSqueezeCenter : undef];
+		push @menu, [string('STOP_SQUEEZECENTER'), $state == SC_STATE_RUNNING ? \&stopSqueezeCenter : undef];
 	}
-	elsif ($scActive) {
+	elsif ($state == SC_STATE_RUNNING) {
 		push @menu, [sprintf('*%s', string('OPEN_SQUEEZECENTER')), \&openSqueezeCenter];
 		push @menu, ["--------"];
 		push @menu, [string('STOP_SQUEEZECENTER'), \&stopSqueezeCenter];
 	}
-	elsif ($starting) {
+	elsif (getServiceState() == SC_STATE_STARTING) {
 		push @menu, [string('STARTING_SQUEEZECENTER'), ""];
 	}
 	else {
 		push @menu, [sprintf('*%s', string('START_SQUEEZECENTER')), \&startSqueezeCenter];
 	}
 
-	my $appString     = string('RUN_AT_LOGIN');
+	my $appString = string('RUN_AT_LOGIN');
 
-	my $setNone  = sub { setStartupType('none') };
-	my $setLogin = sub { setStartupType('login') };
+	my $setNone  = sub { Slim::Utils::OS::Win32::ServiceMgr->setStartAtLogin(0) };
+	my $setLogin = sub { Slim::Utils::OS::Win32::ServiceMgr->setStartAtLogin(1) };
 
-	if ($type eq 'login') {
+	if ($type == SC_STARTUP_TYPE_LOGIN) {
 		push @menu, ["v $appString", $setNone, 1];
 	}
-	elsif ($type ne 'auto') {
+	elsif ($type == SC_STARTUP_TYPE_SERVICE) {
 		push @menu, ["_ $appString", $setLogin, undef];
 	}
 
@@ -123,18 +93,15 @@ sub Singleton {
 
 			$cliStart = 1;
 
-			if (!$scActive && !$starting) {
+			if (getServiceState() == SC_STATE_STOPPED) {
 
 				startSqueezeCenter();
 			}
 
-			if ($scActive) {
+			if (getServiceState() == SC_STATE_RUNNING) {
 
 				openSqueezeCenter();
 
-			} else {
-
-				$checkHTTP = 1;
 			}
 
 		} elsif ($_[1] eq '--exit') {
@@ -151,7 +118,7 @@ sub Singleton {
 # double click on tray icon - attempt to avoid accidental call of exit
 sub DoubleClick {
 
-	if ($scActive) {
+	if (getServiceState() == SC_STATE_RUNNING) {
 
 		openSqueezeCenter();
 
@@ -168,11 +135,11 @@ sub ToolTip {
 	# use English if HE is selected on western systems, as these can't handle the Hebrew tooltip
 	my $lang = ($language eq 'HE' && Win32::Locale::get_language() ne 'he' ? 'EN' : $language);
 
- 	if ($starting) {
+ 	if (getServiceState() == SC_STATE_STARTING) {
 		$state = string('SQUEEZECENTER_STARTING', $lang);
  	}
  
- 	elsif ($scActive) {
+ 	elsif (getServiceState() == SC_STATE_RUNNING) {
 		$state = string('SQUEEZECENTER_RUNNING', $lang);
  	}
     
@@ -190,17 +157,18 @@ sub ToolTip {
 # and modifies state variables.
 sub Timer {
 
-	checkSCActive();
+	my $state = Slim::Utils::OS::Win32::ServiceMgr->checkServiceState();
 
-	if ($starting) {
+	if ($state == SC_STATE_STARTING) {
 
-		SetAnimation($timerSecs * 1000, 1000, "SqueezeCenter", "SqueezeCenterOff");
+		SetAnimation(TIMERSECS * 1000, 1000, "SqueezeCenter", "SqueezeCenterOff");
 
-	} elsif ($scActive && $checkHTTP && checkForHTTP()) {
+	} elsif ($state == SC_STATE_RUNNING && ($cliStart || $cliInstall)) {
 
-		$checkHTTP = 0;
-		openSqueezeCenter() if ($cliStart || $cliInstall)
+		openSqueezeCenter();
 	}
+
+	checkSCActive();
 }
 
 # The one-time startup timer, since there are things we can't do
@@ -219,25 +187,14 @@ sub checkAndStart {
 	}
 
 	if ($cliInstall) {
-		# preset $atLogin if it isn't defined yet
-		$atLogin = 1 if ($atLogin ne '0' && $atLogin ne '1');
-
-		$Registry->{'CUser/Software/'}->{'Logitech/'} = {
-			'SqueezeCenter/' => {
-				'/StartAtLogin' => $atLogin
-			},
-		};
+		Slim::Utils::OS::Win32::ServiceMgr->initStartupType();
 
 		checkSCActive();
-
-		$checkHTTP = 1; # check server and open browser when it comes up
 	}
-
-	my $startupType = startupType();
 
 	# If we're set to Start at Login, do it, but only if the process isn't
 	# already running.
-	if (processID() == -1 && $startupType eq 'login') {
+	if (processID() == -1 && getStartupType() == SC_STARTUP_TYPE_LOGIN) {
 
 		startSqueezeCenter();
 	}
@@ -248,80 +205,39 @@ sub checkAndStart {
 	# Handle the command line --start flag.
 	if ($cliStart) {
 
-		if (!$scActive && !$starting) {
+		my $state = getServiceState();
+
+		if ($state == SC_STATE_STOPPED) {
 
 			startSqueezeCenter();
 		}
 
-		if ($scActive) {
+		if ($state == SC_STATE_RUNNING) {
 
 			openSqueezeCenter();
 
-		} else {
-
-			$checkHTTP = 1;
 		}
 
 	}
 }
 
 sub checkSCActive {
-	my $state = 'stopped';
+	my $update = shift;
+	
+	Slim::Utils::OS::Win32::ServiceMgr->checkServiceState();
+	
+	my $state = getServiceState();
 
-	if (startupTypeIsService()) {
-
-		my %status = ();
-
-		Win32::Service::GetStatus('', $serviceName, \%status);
-
-		if ($status{'CurrentState'} == 0x04) {
-
-			$state = 'running';
-		}
-
-		if ($status{'CurrentState'} == 0x02) {
-
-			$starting = 1;
-		}
-
-		if ($status{'CurrentState'} == 0x01) {
-
-			$starting = 0;
-		}
-
-	} else {
-
-		if (processID() != -1) {
-
-			$state = 'running';
-		}
-
-	}
-
-	if ($state eq 'running' && $checkHTTP && !checkForHTTP()) {
-
-		$starting = 1;
-		$scActive = 0;
-
-	}
-	elsif ($state eq 'running') {
-
+	if ($state == SC_STATE_RUNNING) {
 		SetIcon("SqueezeCenter");
-		$scActive = 1;
-		$starting = 0;
-
-	} else {
-
+	}
+	else {
 		SetIcon("SqueezeCenterOff");
-		$scActive = 0;
 	}
 }
 
 # see whether SC has downloaded an update version
 sub checkForUpdate {
-	# only check when SC is running
-	return if !$scActive;
-
 	if ( _getUpdateInstaller() ) {
 		Balloon(string('UPDATE_AVAILABLE'), "SqueezeCenter", "", 1);
 		
@@ -336,17 +252,13 @@ sub _getUpdateInstaller {
 }
 
 sub startSqueezeCenter {
-	return if startupTypeIsService();
+	Slim::Utils::OS::Win32::ServiceMgr->start();
 
-	runBackground($appExe);
-
-	if (!$scActive) {
+	if (getServiceState() != SC_STATE_STARTING) {
 
 		Balloon(string('STARTING_SQUEEZECENTER'), "SqueezeCenter", "", 1);
-		SetAnimation($timerSecs * 1000, 1000, "SqueezeCenter", "SqueezeCenterOff");
+		SetAnimation(TIMERSECS * 1000, 1000, "SqueezeCenter", "SqueezeCenterOff");
 
-		$checkHTTP = 1;
-		$starting = 1;
 	}
 }
 
@@ -354,7 +266,7 @@ sub startSqueezeCenter {
 sub openSqueezeCenter {
 
 	# Check HTTP first in case SqueezeCenter has changed the HTTP port while running
-	my $serverUrl = checkForHTTP();	
+	my $serverUrl = Slim::Utils::OS::Win32::ServiceMgr->checkForHTTP();	
 	Execute($serverUrl) if $serverUrl;
 
 	$cliStart = 0;
@@ -366,215 +278,14 @@ sub showErrorMessage {
 	MessageBox($message, "SqueezeCenter", MB_OK | MB_ICONERROR);
 }
 
-sub startupTypeIsService {
-	return (startupType() eq 'auto');
-}
-
-# Determine how the user wants to start SqueezeCenter
-sub startupType {
-	my %services;
-
-	Win32::Service::GetServices('', \%services);
-
-	if (grep {$services{$_} =~ /$serviceName/} keys %services) {
-		return 'auto';
-	}
-
-	if ($atLogin) {
-		return 'login';
-	}
-
-	return 'none';
-}
-
-sub setStartupType {
-	my $type = shift;
-
-	if ($type !~ /^(?:login|none)$/) {
-
-		return;
-	}
-
-	if ($type eq 'login') {
-
-		$Registry->{"$registryKey/StartAtLogin"} = $atLogin = 1;
-
-	} elsif ($type eq 'none') {
-
-		$Registry->{"$registryKey/StartAtLogin"} = $atLogin = 0;
-	}
-}
-
-# Return the SqueezeCenter install directory.
-sub installDir {
-
-	# Try and find it in the registry.
-	# This is a system-wide registry key.
-	my $swKey = $Registry->{"LMachine/Software/Logitech/SqueezeCenter/Path"};
-
-	if (defined $swKey) {
-		return $swKey;
-	}
-
-	# Otherwise look in the standard location.
-	# search in legacy SlimServer folder, too
-	my $installDir;
-	PF: foreach my $programFolder ($ENV{ProgramFiles}, 'C:/Program Files') {
-		foreach my $ourFolder ('SqueezeCenter', 'SlimServer') {
-
-			$installDir = File::Spec->catdir($programFolder, $ourFolder);
-			last PF if (-d $installDir);
-
-		}
-	}
-
-	# If it's not there, use the current working directory.
-	if (!-d $installDir) {
-
-		$installDir = cwd();
-	}
-
-	return $installDir;
-}
-
-# Return directory for files which SqueezeCenter can save - i.e. location of prefs file
-sub writableDir {
-	my $writablePath;
-
-	# the installer is writing the data folder to the registry - give this the first try
-	my $swKey = $Win32::TieRegistry::Registry->Open(
-		'LMachine/Software/Logitech/SqueezeCenter/', 
-		{ 
-			Access => Win32::TieRegistry::KEY_READ(), 
-			Delimiter =>'/' 
-		}
-	);
-	
-	if (defined $swKey && $swKey->{'DataPath'}) {
-		$writablePath = $swKey->{'DataPath'};
-	}
-
-	else {
-		# second attempt: use the Windows API (recommended by MS)
-		# use the "Common Application Data" folder to store SqueezeCenter configuration etc.
-		$writablePath = Win32::GetFolderPath(Win32::CSIDL_COMMON_APPDATA);
-			
-		# fall back if no path or invalid path is returned
-		if (!$writablePath || $writablePath eq Win32::GetFolderPath(0)) {
-
-			# third attempt: read the registry's compatibility value
-			# NOTE: this key has proved to be wrong on some Vista systems
-			# only here for backwards compatibility
-			$swKey = $Win32::TieRegistry::Registry->Open(
-				'LMachine/Software/Microsoft/Windows/CurrentVersion/Explorer/Shell Folders/', 
-				{ 
-					Access => Win32::TieRegistry::KEY_READ(), 
-					Delimiter =>'/' 
-				}
-			);
-			
-			if (defined $swKey && $swKey->{'Common AppData'}) {
-				$writablePath = $swKey->{'Common AppData'};
-			}
-				
-			elsif ($ENV{'ProgramData'}) {
-				$writablePath = $ENV{'ProgramData'};
-			}
-
-			# this point hopefully is never reached, as on most systems the program folder isn't writable...
-			else {
-				$writablePath = File::Spec->catdir(installDir(), 'server');
-			}
-		}
-			
-		$writablePath = File::Spec->catdir($writablePath, 'SqueezeCenter');
-	}
-
-	return $writablePath;
-}
-
-# Read pref from the server preference file - lighter weight than loading YAML
-sub getPref {
-	my $pref = shift;
-	my $prefFile = shift;
-	
-	if ($prefFile) {
-		$prefFile = File::Spec->catdir(writableDir(), 'prefs', 'plugin', $prefFile);
-	}
-	else {
-		$prefFile = $serverPrefFile;
-	}
-
-	my $ret;
-
-	if (-r $prefFile) {
-
-		if (open(PREF, $prefFile)) {
-
-			while (<PREF>) {
-			
-				# read YAML (server) and old style prefs (installer)
-				if (/^$pref(:| \=)? (.+)$/) {
-					$ret = $2;
-					last;
-				}
-			}
-
-			close(PREF);
-		}
-	}
-
-	return $ret;
-}
-
-sub checkForHTTP {
-	my $httpPort = getPref('httpport') || 9000;
-
-	# Use low-level socket code. IO::Socket returns a 'Invalid Descriptor'
-	# erorr. It also sucks more memory than it should.
-	my $raddr = '127.0.0.1';
-	my $rport = $httpPort;
-
-	my $iaddr = inet_aton($raddr);
-	my $paddr = sockaddr_in($rport, $iaddr);
-
-	socket(SSERVER, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
-
-	if (connect(SSERVER, $paddr)) {
-
-		close(SSERVER);
-		return "http://$raddr:$httpPort";
-	}
-
-	return 0;
-}
-
-sub runBackground {
-	my @args = @_;
-
-	$args[0] = Win32::GetShortPathName($args[0]);
-
-	Win32::Process::Create(
-		$processObj,
-		$args[0],
-		"@args",
-		0,
-		DETACHED_PROCESS | CREATE_NO_WINDOW | NORMAL_PRIORITY_CLASS,
-		'.'
-	);
-}
-
 sub processID {
+	my $pid = Slim::Utils::OS::Win32::ServiceMgr->getProcessID();
 
-	my $p = Win32::Process::List->new;
-
-	if ($p->IsError == 1) {
-
-		showErrorMessage("ProcessID: an error occured: " . $p->GetErrorText . " ");
+	# if there was an error, getProcessID() will return the error mesasage
+	if ($pid =~ /[^\d\-]/) {
+		showErrorMessage("ProcessID: an error occured: $pid");
+		return -1;
 	}
-
-	# Windows sometimes only displays squeez~1.exe or similar
-	my $pid = ($p->GetProcessPid(qr/^squeez(ecenter|~\d).exe$/i))[1];
 
 	return $pid if defined $pid;
 	return -1;
@@ -601,7 +312,7 @@ sub launchCleanup {
 		stopSqueezeCenter();
 	}
 	
-	Execute(File::Spec->catdir(installDir(), 'server', 'cleanup.exe'));
+	Execute(catdir(Slim::Utils::OS::Win32::ServiceMgr->installDir(), 'server', 'cleanup.exe'));
 }
 
 sub stopSqueezeCenter {
@@ -614,11 +325,10 @@ sub stopSqueezeCenter {
 		return;
 	}
 
-	if ($scActive) {
+	if (getServiceState() == SC_STATE_RUNNING) {
 
 		Balloon(string('STOPPING_SQUEEZECENTER'), "SqueezeCenter", "", 1);
 
-		$scActive = 0;
 	}
 }
 
@@ -668,43 +378,6 @@ sub uninstall {
 	exit;
 }
 
-# return localised version of string token
-sub string {
-	my $name = shift;
-	my $lang = shift || $language;
-
-	$strings{ $name }->{ $lang } || $strings{ $name }->{'EN'} || "Bad string $name";
-}
-
-sub loadStrings {
-	my $string     = '';
-	my $language   = '';
-	my $stringname = '';
-
-	foreach my $line (PerlApp::get_bound_file('strings.txt')) {
-
-		chomp($line);
-		utf8::decode($line);
-		
-		next if $line =~ /^#/;
-		next if $line !~ /\S/;
-
-		if ($line =~ /^(\S+)$/) {
-
-			$stringname = $1;
-			$string = '';
-			next;
-
-		} elsif ($line =~ /^\t(\S*)\t(.+)$/) {
-
-			$language = uc($1);
-			$string   = $2;
-
-			$strings{$stringname}->{$language} = $string;
-		}
-	}
-}
-
 *PerlTray::ToolTip = \&ToolTip;
 
 GetOptions(
@@ -713,8 +386,6 @@ GetOptions(
 	'install'   => \$cliInstall,
 	'uninstall' => \$cliUninstall,
 );
-
-loadStrings();
 
 # Checking for existence & launching of SS in a timer, since it
 # fails if done during Perl initialization.
@@ -726,4 +397,4 @@ SetTimer(":2", \&checkForUpdate);
 # This is our regular timer which continually checks for existence of
 # SS. We could have combined the two timers, but changing the
 # frequency of the timer proved problematic.
-SetTimer(":" . $timerSecs);
+SetTimer(":" . TIMERSECS);
