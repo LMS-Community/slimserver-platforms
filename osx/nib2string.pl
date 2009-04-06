91ccaf41bd87e0f1ebb01b39c4f6fd4eee750a82
Michael Herger <michael@slimdevices.com>
2009-04-06 14:40:40 +0000

    add helper script to extract strings from .nib files
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@25841 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/nib2string.pl
+++ osx/nib2string.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 use strict;
-use File::Copy;
 use File::Spec::Functions qw(catdir);
 use Getopt::Long;
 
@@ -12,13 +11,6 @@ GetOptions(
 	'folder=s' => \$folder,
 	'nib=s'    => \$nib,
 );
-
-my $EN = catdir($folder, 'English.lproj', $nib);
-
-unless (-d $EN && -r $EN) {
-	print "Can't find English original resource '$EN'\n";
-	exit;
-}
 
 my $strings = $nib;
 $strings =~ s/nib$/strings/;
@@ -31,22 +23,15 @@ foreach my $item (readdir(PROJ)) {
 	if ($item =~ /\.lproj$/ && $item !~ /^English/i) {
 		my $stringsFile = catdir($folder, $item, $strings);
 		my $nibFolder   = catdir($folder, $item, $nib);
-		my $nibBackup   = catdir($folder, $item, $nib . '2');
-		
-		unless (-f $stringsFile && -r $stringsFile) {
-			print "Can't find strings file $stringsFile\n";
-			next;
-		}
 		
 		unless (-d $nibFolder && -r $nibFolder) {
 			print "Can't find NIB file $nibFolder\n";
 			next;
 		}
 
-		$nibBackup =~ s/ /\\ /g;
 		$nibFolder =~ s/ /\\ /g;
 
-		`nibtool -d '$stringsFile' '$EN' -W $nibBackup && mv $nibBackup/* $nibFolder && rmdir $nibBackup`;
+		`nibtool -L $nibFolder  > '$stringsFile'`;
 	}
 }
 
