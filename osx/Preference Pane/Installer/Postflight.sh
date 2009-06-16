6e4ab7b4513325696a185b87fef9221e2694721f
Michael Herger <michael@slimdevices.com>
2009-06-16 08:56:31 +0000

    we no longer open the web UI, but the preference pane
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@27099 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/Installer/Postflight.sh
+++ osx/Preference Pane/Installer/Postflight.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-# try to detect SC's web port, and verify whether it's available or not
-# don't try more than 15 seconds
-# open SC web UI in Safari, if SC is available
+/usr/bin/osascript <<EOF
+	tell application "System Preferences"
 
-if [ -e "$HOME/Library/PreferencePanes/Squeezebox Server.prefPane/Contents/Resources" ] ; then
-	cd "$HOME/Library/PreferencePanes/Squeezebox Server.prefPane/Contents/Resources"
-else
-	cd "/Library/PreferencePanes/Squeezebox Server.prefPane/Contents/Resources"
-fi
+		set current pane to pane id "com.slimdevices.slim"
 
-PORT=`./check-web.pl`
-i=0
+		activate
 
-while [ $PORT -eq 0 ] && [ $i -lt 15 ]
-do
-	i=`expr $i + 1`
-	sleep 1
-	
-	PORT=`./check-web.pl`	
-done
-
-if [ $PORT -ne 0 ]; then
-	osascript ../server/Bin/darwin/openprefs.scpt &
-fi
+	end tell
+EOF
