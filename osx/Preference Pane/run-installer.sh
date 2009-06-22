624e8c53cecbc32759590fe13709adce9cf52e16
Michael Herger <michael@slimdevices.com>
2009-06-22 08:38:45 +0000

    need to wrap the installer file path in quotes
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@27188 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/run-installer.sh
+++ osx/Preference Pane/run-installer.sh
@@ -3,10 +3,10 @@
 MOUNTPOINT=/Volumes/SCInstaller
 INSTALLER="/Volumes/SCInstaller/Squeezebox Server Installer.app"
 
-if [ -e $1 ] ; then
+if [ -e "$1" ] ; then
 	xattr -d com.apple.quarantine $1
 	hdiutil unmount $MOUNTPOINT
-	hdiutil mount $1 -mountpoint $MOUNTPOINT
+	hdiutil mount "$1" -mountpoint $MOUNTPOINT
 fi
 
 if [ -e "$INSTALLER" ] ; then
