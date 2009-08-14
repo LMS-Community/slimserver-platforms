6dd9b35f6560d5faf708c49286acebe999adb364
Matt Wise <mwise@slimdevices.com>
2009-08-14 18:22:32 +0000

    Temporary hack that allows us to start up in 32bit mode on Snow Leopard (10.6)
    git-svn-id: file:///home/awy/mirror/slim/7.4/trunk/platforms@28187 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/start-server.sh
+++ osx/Preference Pane/start-server.sh
@@ -2,11 +2,13 @@
 SERVER_RUNNING=`ps -axww | grep "squeezecenter\.pl|slimp3\.pl\|slimp3d\|slimserver\.pl\|slimserver" | grep -v grep | cat`
 if [ z"$SERVER_RUNNING" = z ] ; then
     # Check to see if we're publishing with Rendezvous -- if not, use daemon mode.
-    
+
     if [ z"$#" == z"0" ] ; then
 	if [ ! -e ~/Library/Logs ] ; then mkdir ~/Library/Logs ; fi
-	./slimserver.pl --daemon $1 &> /dev/null &
+        # Temporary workaround for 64bit OSX 10.6 systems, sets perl to run in 32bit mode
+        VERSIONER_PERL_PREFER_32_BIT=yes ./slimserver.pl --daemon $1 &> /dev/null &
     else
-	./slimserver.pl $1 & >> /tmp/squeezeboxerror.log 2>&1
+        # Temporary workaround for 64bit OSX 10.6 systems, sets perl to run in 32bit mode
+        VERSIONER_PERL_PREFER_32_BIT=yes ./slimserver.pl $1 & >> /tmp/squeezeboxerror.log 2>&1
     fi
 fi
