80c951b897105b1e198d14db647723f890e63675
Andy Grundman <agrundman@slimdevices.com>
2009-08-29 15:32:34 +0000

    Remove code to force 32-bit mode
    git-svn-id: file:///home/awy/mirror/slim/7.3/trunk/platforms@28354 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/start-server.sh
+++ osx/Preference Pane/start-server.sh
@@ -5,10 +5,8 @@ if [ z"$SERVER_RUNNING" = z ] ; then
     
     if [ z"$#" == z"0" ] ; then
 	if [ ! -e ~/Library/Logs ] ; then mkdir ~/Library/Logs ; fi
-	# Temporary workaround for 64bit OSX 10.6 systems, sets perl to run in 32bit mode
-        VERSIONER_PERL_PREFER_32_BIT=yes ./slimserver.pl --daemon &> /dev/null &
+        ./slimserver.pl --daemon &> /dev/null &
     else
-	# Temporary workaround for 64bit OSX 10.6 systems, sets perl to run in 32bit mode
-        VERSIONER_PERL_PREFER_32_BIT=yes ./slimserver.pl & >> /tmp/squeezecentererror.log 2>&1
+        ./slimserver.pl & >> /tmp/squeezecentererror.log 2>&1
     fi
 fi
