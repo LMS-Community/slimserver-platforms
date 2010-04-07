fde1822c8189170a64b8ef9b9dcb5e0893758cc6
Michael Herger <mherger@logitech.com>
2010-04-07 09:47:51 +0000

    add osx 10.3 compatible version of the old (<=7.3) PreferencePane. The installer will replace the default binary and resource files on 10.3 systems.
    Bug: 16007
    git-svn-id: file:///home/awy/mirror/slim/7.5/trunk/platforms@30487 62299810-d8cb-41fd-93b7-d32162e5a4a4

--- osx/Preference Pane/Installer/install.sh
+++ osx/Preference Pane/Installer/install.sh
@@ -103,6 +103,14 @@ if [ -e "$PREFPANE_TO" ] ; then
 
 	sudo -b -H -u $USER "../Resources/start-server.sh"
 
+	# if we're on OSX 10.3 we'll use an old version of the prefpane...	
+	if [ `sw_vers -productVersion | grep -o "^10\.3"` ] ; then
+		cd "$PREFPANE_TO/Contents/"
+		rm -rf Resources/*lproj
+		rm -f MacOS/Squeezebox
+		cp -r 10.3/* .
+	fi
+
 	echo "Squeezebox Server installed successfully."
 	exit 0
 else
