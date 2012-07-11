43d2a306ba82833e7fc830d3174c28dcc69b2fc6
mherger <mherger@logitech.com>
2012-07-11 15:25:17 +0200

    Modify OSX update helper script to use new installation package
    Change-Id: Ie7b7ba401b2fd48f7fa6ea70f006b43ac638d778

--- osx/Preference Pane/run-installer.sh
+++ osx/Preference Pane/run-installer.sh
@@ -2,16 +2,20 @@
 
 # this script is being called from the PrefPane to launch the installer
 
-MOUNTPOINT=/Volumes/SCInstaller
-INSTALLER="/Volumes/SCInstaller/.install.app"
+UPDATEFOLDER=`dirname $1`
+PRODUCT_PREFIX=UEMusicLibrary
+INSTALLER="$UPDATEFOLDER/$PRODUCT_PREFIX*.pkg"
+
+# clean up remnants of earlier installations
+rm -rf $UPDATEFOLDER/$PRODUCT_PREFIX*.pkg
 
 if [ -e "$1" ] ; then
-	xattr -d com.apple.quarantine $1
-	hdiutil unmount $MOUNTPOINT
-	hdiutil mount "$1" -mountpoint $MOUNTPOINT
+	unzip -oq $1 -d $UPDATEFOLDER
+
+	xattr -d com.apple.quarantine $INSTALLER &> /dev/null
 fi
 
-if [ -e "$INSTALLER" ] ; then
-	open "$INSTALLER"
+if [ -e $INSTALLER ] ; then
+	open $INSTALLER
 fi
 	
