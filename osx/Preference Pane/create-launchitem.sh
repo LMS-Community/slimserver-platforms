47af95276fbcca10d359fb49cd91e1dafd5d926c
mherger <mherger@logitech.com>
2012-11-01 12:06:33 +0100

    64-bit Preference Pane for OSX
    - will only run on Intel CPUs, OSX 10.5+
    - replace JSON framework with more recent version supporting both 64 &
    32 bits
    - remove helper apps - backport new scripts from 10.0 instead
    
    Change-Id: Ie1b085ca64f719158ee166ccac2dd1854ba894cd

---
+++ osx/Preference Pane/create-launchitem.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Create the LaunchAgent item for the server.
+
+PRODUCT_NAME=UEMusicLibrary
+PRODUCT_PLIST="$HOME/Library/LaunchAgents/$PRODUCT_NAME.plist"
+
+if [ -e "$HOME/Library/PreferencePanes/PRODUCT_NAME.prefPane/Contents/server" ] ; then
+	PRODUCT_FOLDER="$HOME/Library/PreferencePanes/$PRODUCT_NAME.prefPane/Contents/server"
+else
+	PRODUCT_FOLDER="/Library/PreferencePanes/$PRODUCT_NAME.prefPane/Contents/server"
+fi
+
+launchctl unload $PRODUCT_PLIST &> /dev/null
+
+cat >$HOME/Library/LaunchAgents/$PRODUCT_NAME.plist << !!
+<plist version="1.0">
+	<dict>
+		<key>Label</key>
+		<string>UEMusicLibrary</string>
+		<key>RunAtLoad</key>
+		<true />
+		<key>Program</key>
+		<string>$PRODUCT_FOLDER/ueml.pl</string>
+		<key>WorkingDirectory</key>
+		<string>$PRODUCT_FOLDER</string>
+	</dict>
+</plist>
+!!
+
+launchctl load $PRODUCT_PLIST &> /dev/null
---
+++ osx/Preference Pane/remove-launchitem.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Create the LaunchAgent item for the server.
+
+PRODUCT_NAME=Squeezebox
+PRODUCT_PLIST="$HOME/Library/LaunchAgents/$PRODUCT_NAME.plist"
+
+launchctl unload $PRODUCT_PLIST &> /dev/null
+
+rm -f $PRODUCT_PLIST &> /dev/null
