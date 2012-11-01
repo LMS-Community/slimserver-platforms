dfbffc185d9d8052c95d2f2a0ee02dfdda3a2f2d
mherger <mherger@logitech.com>
2012-11-01 12:06:33 +0100

    64-bit Preference Pane for OSX
    - will only run on Intel CPUs, OSX 10.5+
    - replace JSON framework with more recent version supporting both 64 &
    32 bits
    - remove helper apps - backport new scripts from 10.0 instead
    
    Change-Id: I572f0f50730871f1ff192112559ebbac753c8a9d

--- osx/Preference Pane/stop-server.sh
+++ osx/Preference Pane/stop-server.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 SERVER_RUNNING=`ps -axww | grep "squeezecenter\.pl|slimp3\.pl\|slimp3d\|slimserver\.pl\|slimserver" | grep -v grep | cat`
 if [ z"$SERVER_RUNNING" != z ] ; then
 	kill `echo $SERVER_RUNNING | sed -n 's/^[ ]*\([0-9]*\)[ ]*.*$/\1/p'`
