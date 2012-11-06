63de591e2e4bd6f70664e489f5847e32b94b4130
mherger <mherger@logitech.com>
2012-11-05 17:49:55 -0800

    The installer is no longer a zip file - no need to unzip
    Change-Id: I44bbca1807f9bbc425dd796bb8670e799c019a19

--- osx/Preference Pane/run-installer.sh
+++ osx/Preference Pane/run-installer.sh
@@ -9,13 +9,8 @@ INSTALLER="$UPDATEFOLDER/$PRODUCT_PREFIX*.pkg"
 # clean up remnants of earlier installations
 rm -rf $UPDATEFOLDER/$PRODUCT_PREFIX*.pkg
 
-if [ -e "$1" ] ; then
-	unzip -oq $1 -d $UPDATEFOLDER
-
-	xattr -d com.apple.quarantine $INSTALLER &> /dev/null
-fi
-
 if [ -e $INSTALLER ] ; then
+	xattr -d com.apple.quarantine $INSTALLER &> /dev/null
 	open $INSTALLER
 fi
 	
