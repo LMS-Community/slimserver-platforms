17c8247519d9a83acd887ec76e046ac1d3f83c55
mherger <mherger@logitech.com>
2013-09-20 14:24:52 +0200

    Remove a dependency from the xml helper script

--- readynas/addons_sdk/bin/xmlutil.pl
+++ readynas/addons_sdk/bin/xmlutil.pl
@@ -3,20 +3,18 @@
 use strict;
 use warnings;
 
-#use Data::Dump;
-use File::Spec::Functions qw(splitdir);
 use XML::Simple qw(XMLin);
 
 my ($file, $target) = @ARGV;
 
-if ( !(-f $file && -r _) ) {
+if ( !$file || !(-f $file && -r _) ) {
 	warn "Can't read file '$file' or file not found\n";
 	exit;
 }
 
 my $data = XMLin($file);
 
-my @path = grep { $_ && $_ !~ /addon/ } splitdir($target);
+my @path = grep { $_ && $_ !~ /addon/ } split m|/|, $target;
 
 print $path[0] . '!!' . $data->{$path[0]};
 
