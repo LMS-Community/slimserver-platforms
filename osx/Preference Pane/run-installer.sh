4c24a5a56e2b181509b029c2226a0d4a4cfd42de
Michael Herger <mherger@logitech.com>
2015-08-27 17:18:20 +0200

    Log some information about the LMS installer when run from the pref pane

--- osx/Preference Pane/run-installer.sh
+++ osx/Preference Pane/run-installer.sh
@@ -2,9 +2,19 @@
 
 # this script is being called from the PrefPane to launch the installer
 
+syslog -s -k Facility com.apple.console \
+             Level Error \
+             Sender "Logitech Media Server installer" \
+             Message "Launching: $1"
+
 if [ -e $1 ] ; then
 	xattr -d com.apple.quarantine $1 &> /dev/null
 
 	open $1
+else
+	syslog -s -k Facility com.apple.console \
+	             Level Error \
+	             Sender "Logitech Media Server installer" \
+	             Message "Didn't find $1"
 fi
 	
