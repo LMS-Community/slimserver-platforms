100f36db317e94066b19b1fb4dbf6ba15ab9d1c7
Michael Herger <mherger@logitech.com>
2020-10-23 14:32:29 +0200

    Make LMS' httpport configurable through environment variable
    The port used in the container needs to be the same as mapped outside, as its part of the slimproto announcement used by players to connect to the server. Therefore a simple mapping from default 9000 internally to 9002 outside would not allow a player to connect to a container run on eg. Synology NAS where port 9000 is occupied.

--- Docker/start-container.sh
+++ Docker/start-container.sh
@@ -8,9 +8,8 @@ PGID=${PGID:-`id -g squeezeboxserver`}
 usermod -o -u "$PUID" squeezeboxserver
 groupmod -o -g "$PGID" nogroup
 
-umask 0002
-
 #Add permissions
 chown -R squeezeboxserver:nogroup /config /playlist /lms
 
-su squeezeboxserver -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache'
+echo Starting Logitech Media Server on port $HTTP_PORT...
+su squeezeboxserver -c '/usr/bin/perl /lms/slimserver.pl --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache --httpport $HTTP_PORT'
