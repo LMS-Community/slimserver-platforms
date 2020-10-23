547cf5bdb15cdae38ccfd94c8ad1a456360a78bf
snoopy86 <8215353+snoopy86@users.noreply.github.com>
2020-10-23 08:29:04 +0200

    Update permissions

--- Docker/start-container.sh
+++ Docker/start-container.sh
@@ -3,14 +3,14 @@
 #Set user and group
 umask 0002
 PUID=${PUID:-`id -u squeezeboxserver`}
-PGID=${PGID:-`id -g squeezeboxserver`}
+PGID=${PGID:-`id -g user`}
 
 usermod -o -u "$PUID" squeezeboxserver
-groupmod -o -g "$PGID" squeezeboxserver
+groupmod -o -g "$PGID" user
 
 umask 0002
 
 #Add permissions
-chown -R squeezeboxserver:users /config /playlist
+chown -R squeezeboxserver:users /config /playlist /music
 
 /usr/bin/perl /lms/slimserver.pl --user squeezeboxserver --prefsdir /config/prefs --logdir /config/logs --cachedir /config/cache
