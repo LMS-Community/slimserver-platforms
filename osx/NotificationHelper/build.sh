6302a38fc5eeea6b0552f9360b236b847a43c795
Michael Herger <mherger@logitech.com>
2024-09-18 17:19:52 +0200

    Add a helper script to show notifications.

---
+++ osx/NotificationHelper/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+platypus \
+--name="lms-notify" \
+--interface-type=None \
+--author="Lyrion Community, Michael Herger" \
+--app-version=1.0.0 \
+--app-icon=../Preference\ Pane/icon.icns \
+--overwrite \
+NotificationHelper.sh
